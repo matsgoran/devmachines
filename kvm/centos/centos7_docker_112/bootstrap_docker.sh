daa1a0b030a159c5c38483c51f7a0677a33adaaf
Mats-Gøran Karlsen <mats.goran.karlsen@bibsys.no>
2017-09-04 10:52:02 +0200

    Add cpus and ram. Bugfix docker install

--- kvm/centos/centos7_docker_112/bootstrap_docker.sh
+++ kvm/centos/centos7_docker_112/bootstrap_docker.sh
@@ -8,7 +8,7 @@ rpm --import "https://sks-keyservers.net/pks/lookup?op=get&search=0xee6d536cf7dc
 yum install -y yum-utils
 yum-config-manager --add-repo https://packages.docker.com/1.12/yum/repo/main/centos/7
 yum makecache fast
-yum install docker-engine
+yum install -y docker-engine
 # Start docker
 systemctl enable docker.service && systemctl start docker.service
 # Add vagrant to docker group
