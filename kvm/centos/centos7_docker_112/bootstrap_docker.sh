4c3f1e1536ac222343385cf6cdc45c7450c050a4
Mats-Gøran Karlsen <mats.goran.karlsen@bibsys.no>
2017-09-04 10:49:51 +0200

    Add Docker public key for Docker engine packages

--- kvm/centos/centos7_docker_112/bootstrap_docker.sh
+++ kvm/centos/centos7_docker_112/bootstrap_docker.sh
@@ -4,6 +4,7 @@
 #Update yum
 yum update
 # Docker 1.12 installation, https://docs.docker.com/cs-engine/1.12/#install-using-a-repository
+rpm --import "https://sks-keyservers.net/pks/lookup?op=get&search=0xee6d536cf7dc86e2d7d56f59a178ac6c6238f52e"
 yum install -y yum-utils
 yum-config-manager --add-repo https://packages.docker.com/1.12/yum/repo/main/centos/7
 yum makecache fast
